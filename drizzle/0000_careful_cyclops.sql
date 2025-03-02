a723312d1931f83c6590434dba1a35be935ea818
zync <chandankrishna288@gmail.com>
2025-03-02 21:00:32 +0530

    added features like filtering and new pages

--- drizzle/0000_careful_cyclops.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `expenses` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`title` text NOT NULL,
-	`category` text NOT NULL,
-	`date` text NOT NULL,
-	`price` integer NOT NULL
-);
